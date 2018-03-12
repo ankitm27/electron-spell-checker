1b5d325071ed8cd96a9f069aae90dc91f47950cd
ankitm27 <ankit.mahotra00@gmail.com>
2018-03-12 13:09:36 +0530

    chnages

--- src/server.js
+++ src/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
 const spell = require('spellchecker');
-
-
 app.get('/api', (req, res) => {
-    console.log("check");
-    console.log("req.query",req.query);
     res.send({"check":!spell.isMisspelled(req.query.text)})
 });
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
-})
+});
